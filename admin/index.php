fca2d08c1ec596a3bec1cfa188c4d4c2ff811496
Ajay Kumar <ajayjatav439@gmail.com>
2026-06-07 18:24:30 +0530

    Remove admin folder

--- admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Front controller when the web server document root is the project root.
- * Prefer pointing the vhost document root at /public when possible.
- */
-require __DIR__.'/public/index.php';
